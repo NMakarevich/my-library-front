7df78279f0326e0e3a658b132d75cb79deac21b2
NMakarevich <nikoladze529@mail.ru>
2023-07-19 23:24:54 +0300

    feat: add layout based on created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BurgerButtonModule } from './components/UI/burger-button/burger-button.module';
+import { NgOptimizedImage } from '@angular/common';
+import { SidenavModule } from './components/UI/sidenav/sidenav.module';
+import { NavItemModule } from './components/UI/nav-item/nav-item.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BurgerButtonModule,
+    NgOptimizedImage,
+    SidenavModule,
+    NavItemModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
